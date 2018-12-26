134ebefb663bfceb7b7e63f7ae56e211da3cbc28
Muhammet Varol <vrl.muhammet@gmail.com>
2018-12-26 17:03:35 +0300

    İndex.pug oluşturuldu

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
-
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 app.set('view engine', 'pug');
+
+app.use(bodyParser.json())
 
 app.get('/', function (req, res) {
     res.render('index')
   })
+
+  
+app.get('/zirve', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  res.render('zirve')
+})
+
+app.post('/zirve', function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  res.render(JSON.stringify(req.body));
+})
 
 
 app.listen(3000, () => {
